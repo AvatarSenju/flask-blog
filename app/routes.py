61cf97c597898b02eaeef6abc984afa46c24d902
Anushrut <anushrut5@gmail.com>
2019-01-22 20:26:58 +0530

    added pagination feature

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template,flash, redirect, url_for,request
 from app import app,db
-from app.forms import LoginForm,RegistrationForm, EditProfileForm
+from app.forms import LoginForm,RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 
@@ -15,21 +15,29 @@ def before_request():
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {"username" : "Anushrut"}
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url,prev_url=prev_url)
 
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-
-    return render_template('index.html',title="clg", posts=posts)
+ @app.route('/explore')
+ @login_required
+ def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,next_url=next_url,prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -77,15 +85,14 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/user/<username>')
-# this username is going a s a parameter for the following function
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/edit_profile',methods=['GET','POST'])
--- config.py
+++ config.py
@@ -15,3 +15,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['anushrut5@gmail.com']
+
+    POSTS_PER_PAGE = 5 
