3a2e400ac3a2882adf7b9f188b9427bd6f4f5220
Anushrut <anushrut5@gmail.com>
2019-01-24 00:09:37 +0530

    error corrected in reset password email forwarding

--- app/auth/email.py
+++ app/auth/email.py
@@ -8,5 +8,5 @@ def send_password_reset_email(user):
     send_email('[Flask Project] Reset Your Password',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
-               text_body=render_template('email/reset_password.txt', user=use, token=token),
+               text_body=render_template('email/reset_password.txt', user=user, token=token),
                html_body=render_template('email/reset_password.html', user=user, token=token))
