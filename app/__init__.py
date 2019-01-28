9741e851bbef7f7d881ac9a5a71e72777ab0f7be
Anushrut <anushrut5@gmail.com>
2019-01-28 23:49:34 +0530

    minor bug fixed

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from config import Config
+import cv2
 
 db = SQLAlchemy()
 migrate = Migrate()
